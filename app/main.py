b5b90ddd75f0823676c8132b2960c511c76c9a60
Sasha Semenenko <semenenko233249@gmail.com>
2023-04-27 19:39:48 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import json
 from app.customer import Customer
 from app.shop import Shop
-from datetime import datetime
 
 
 def shop_trip():
@@ -10,4 +9,15 @@ def shop_trip():
 
         fuel_price = file_json["FUEL_PRICE"]
         customers = file_json["customers"]
-        shops = file_json["shops"]
+        shops = [Shop(shop) for shop in file_json["shops"]]
+
+        for customer in customers:
+            one_customer = Customer(customer)
+
+            best_shop, best_price = one_customer.cheap_store(fuel_price, shops)
+            if best_price >= one_customer.money:
+                print(f"{one_customer.name} doesn't gave enough money to make a purchase in any shop")
+                break
+            one_customer.shopping(best_shop, fuel_price, best_price)
+            best_shop.purchase_receipt(one_customer.name, one_customer.product)
+            one_customer.way_home()
