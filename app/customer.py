604188179cec801542fbbda95d5edf425a8260d1
Sasha Semenenko <semenenko233249@gmail.com>
2023-05-02 10:50:39 +0300

    Solution

--- app/customer.py
+++ app/customer.py
@@ -8,7 +8,6 @@ class Customer:
         self.name: str = customer["name"]
         self.product_cart: dict = customer["product_cart"]
         self.location: list = customer["location"]
-        self.home_location: list = customer["location"]
         self.money: int = customer["money"]
         self.car: dict = customer["car"]
 
@@ -17,8 +16,8 @@ class Customer:
         return self.car["fuel_consumption"] * distance / 100 * fuel_price
 
     def product_cost(self, product_shop: dict) -> float:
-        return sum([product_shop[product] * cost
-                    for product, cost in self.product_cart.items()])
+        return sum(product_shop[product] * cost
+                   for product, cost in self.product_cart.items())
 
     def cheap_store(self, fuel_price: float, shops: List[Shop]) -> tuple:
         print(f"{self.name} has {self.money} dollars")
@@ -29,7 +28,8 @@ class Customer:
             total_cost = round((fuel_cost + product_price), 2)
             cheap_shop_price[total_cost] = shop
             print(f"{self.name}'s trip to the {shop.name} costs {total_cost}")
-        return cheap_shop_price[min(cheap_shop_price)], min(cheap_shop_price)
+        min_price = min(cheap_shop_price)
+        return cheap_shop_price[min_price], min_price
 
     def shopping(self,
                  shop: Shop,
@@ -42,6 +42,5 @@ class Customer:
         self.money -= product_cost
 
     def way_home(self) -> None:
-        self.location = self.home_location
         print(f"{self.name} rides home\n"
               f"{self.name} now has {self.money} dollars\n")
